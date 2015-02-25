46b31527cde61e12f333052b6c4b55a06d84072e
ccooper <ccooper@noemail.ucla.edu>
2015-02-25 21:16:35 +0000

    removed checking of default OICR path for bedtools, bedops, and tabix, and converted double to single quotes
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@40721 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/check.binary.R
+++ R/check.binary.R
@@ -9,45 +9,34 @@
 # If publications result from research using this SOFTWARE, we ask that the Ontario Institute for Cancer Research be acknowledged and/or
 # credit be given to OICR scientists, as scientifically appropriate.
 
-check.binary <- function(x = "bedtools", verbose = TRUE) {
+check.binary <- function(x = 'bedtools', verbose = TRUE) {
 
-	if (x == "bedtools") {
-		default.path <- "/oicr/local/analysis/sw/bedtools/2.18.2/bin";
-		git.url <- "https://github.com/arq5x/bedtools2";
+	default.path <- '';
+	git.url <- '';
+
+	if ('bedtools' == x) {
+		git.url <- 'https://github.com/arq5x/bedtools2';
 		}
-	else if (x == "bedops") {
-		default.path <- "/oicr/local/boutroslab/sw/bedops/2.2.0/bin";
-		git.url <- "https://github.com/alexpreynolds/bedops";
+	else if ('bedops' == x) {
+		git.url <- 'https://github.com/bedops/bedops';
 		}
-	else if (x == "tabix") {
-		default.path <- "/oicr/local/analysis/sw/tabix/tabix-0.2.6";
-		git.url <- "https://github.com/samtools/tabix";
-		}
-	else {
-		default.path <- "";
-		git.url <- "";
+	else if ('tabix' == x) {
+		git.url <- 'https://github.com/samtools/tabix';
 		}
 
-	catv(paste0("  * Checking path for ", x, "... ") );
+	catv(paste0('  * Checking path for ', x, '... ') );
 
 	# check if binary is in path
-	if (Sys.which(x) == "") {
-		catv(paste0("FAIL\n") );
-		path <- Sys.getenv("PATH");
-		if (file.exists(default.path)) {
-			Sys.setenv(PATH=paste0(path,":",default.path));
-			catv(paste0("  * Checking default locations for ", x, "... PASS\n    ", default.path, "\n") );
-			}
-		else {
-			catv(paste0("  * Checking default locations for ", x, "... FAIL\n    ", default.path, "\n") );
-			if (git.url != "") {
-				catv(paste0("    Download: ", git.url));
-				}
-			return(0);
+	if ('' == Sys.which(x)) {
+		catv(paste0('FAIL\n') );
+		path <- Sys.getenv('PATH');
+
+		if ('' == git.url) {
+			catv(paste0('    It can be downloaded from here: ', git.url, '\n'));
 			}
 		}
 	else {
-		catv(paste0("PASS\n    ", Sys.which(x), "\n") );
+		catv(paste0('PASS\n    ', Sys.which(x), '\n') );
 		}
 
 	return(1);
