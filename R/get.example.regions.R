773d689f083cc69a35cc84a6bb8de990487eedc5
shaider <shaider@noemail.ucla.edu>
2016-07-11 10:31:35 +0000

    updated example regions to support improved demonstration
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@55070 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/get.example.regions.R
+++ R/get.example.regions.R
@@ -14,12 +14,15 @@ get.example.regions <- function() {
 	a <- c("chr1:10-100", "chr1:101-200","chr1:200-210", "chr1:211-212", "chr2:10-50","chr10:50-100", "chr2:40-60","chr20:1-5");
 
 	# nothing special but should overlap with (a)  
-	b <- c("chr1:1-10", "chr1:111-250","chr1:2000-2010", "chr2:1-5","chr10:100-150", "chr2:40-60","chr20:6-10");
+	b <- c("chr1:1-10", "chr1:111-250","chr1:2000-2010", "chr2:1-5", "chr10:100-150", "chr2:40-60","chr20:6-10");
 
 	# incorrect format start>end, strange chr, longer than expected chr
 	d <-  c("chr1:10-100", "chr1:101:200", "chr10:100-101", "1:200-210", "chr2:50-40","chr10:50-50", "chr2:o-60","chr20:1-5x");
 
-	x <- list(a = a, b = b, d = d)
+	# should overlap with a and b
+	e <- c("chr1:1-10", "chr1:120-220","chr1:20-240", "chr2:1-20", "chr10:110-150", "chr2:30-60","chr20:7-12");
+
+	x <- list(a = a, b = b, d = d, e = e)
 
 	return(x);
 	}
