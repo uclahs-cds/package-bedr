3d92abdc93e447b6106fde6d2e2329816762166c
whelena <whelenakanya@yahoo.com>
2021-09-01 16:09:30 -0700

    remove non-DF approach

--- R/vcf2bedpe.R
+++ R/vcf2bedpe.R
@@ -43,156 +43,22 @@ vcf.list <- list()
 
 for (i in 1:length(file.list)) {
   # read the VCF file
-  temp.vcf <- read.vcf(file.list[i], split.info = TRUE)$vcf;
+  temp.vcf <- read.vcf(file.list[i], split.info = TRUE)
   
   # focus on SVs
-  temp.vcf <- temp.vcf[which(temp.vcf$SVTYPE %in% VALID.SV.TYPES), ];
+  #temp.vcf <- temp.vcf[which(temp.vcf$SVTYPE %in% VALID.SV.TYPES), ];
   
   # convert to zero-based coordinates
-  temp.vcf$POS <- temp.vcf$POS - 1;
+  temp.vcf$vcf$POS <- temp.vcf$vcf$POS - 1;
   
   vcf.list[[names(file.list)[i]]] <- temp.vcf
 }
 
-## vcf to bedpe
-# CHROM_A START_A END_A CHROM_B START_B END_B ID QUAL STRAND_A STRAND_B INFO_A INFO_B
-# 1	23559191	23559192	1	23559244	23559245	DEL_gridss2fb_2966o	2355.69	+	-
 
-
-BL.vcf <- vcf.list[['BL']]
-LCR.vcf <- vcf.list[['LCR']]
-LCR.vcf$ID <- gsub('(.*_)(.*_.*)', '\\2', LCR.vcf$ID)
-
-callerA.bed <- vcf2bed(read.vcf(file.list[1], split.info = TRUE))
-
-## get pairs
-
-x = apply(LCR.vcf[1:5,], 2, function(x) print(x['SVTYPE']))
-x = BL.vcf[5,]
-
-# for BL
-converter_vcf2bedpe <- function(var1, var2 = NULL, alt = FALSE) {
-  x <- var1
-  y <- ifelse(is.null(var2), var1, var2)
-  
-  x$END <- ifelse(is.na(x$END), x$START, x$END)
-  y$END <- ifelse(is.na(y$END), y$START, y$END)
-  
-  coordsA <- adjust_coordinates(x, 'CIPOS', y$POS, x$POS)
-  coordsB <- adjust_coordinates(y, 'CIEND', y$END, y$END)
-  strandA <- '+'
-  strandB <- '-'
-  
-  name <- x$ID
-  if ('EVENT' %in% names(x)) { 
-    name  <- x$EVENT 
-  } else if ('MATEID' %in% names(x) & grepl('^Manta', x$ID)) {
-    name <- str_split(x$ID , ':.$')
-  }
-  
-  if (!is.null(x$MATESTRAND) & !is.na(x$MATESTRAND)) { strandB <- x$MATESTRAND }
-  if (!is.null(y$MATESTRAND) & !is.na(y$MATESTRAND)) { strandA <- y$MATESTRAND }
-  
-  df_row <- data.frame(CHROM_A = x$CHROM, START_A = coordsA[1] , END_A = coordsA[2],
-                       CHROM_B = y$CHROM, START_B = coordsB[1], END_B = coordsB[2],
-                       ID = name, QUAL = var1$QUAL, STRAND_A = strand_A, STRAND_B = strand_B)
-}
-
-vcf2bedpe <- function(vcfDF, bedpe_file) {
-  bnds <- list()
-  sec_bnds <- list()
-  
-  for (i in 1:nrow(vcfDF)) {
-    x <- vcfDF[1, ];
-    
-    
-    if (x$SVTYPE != 'BND' & is.na(x$MATEID)) {
-      bedpe_row <- converter_vcf2bedpe(var1 = x)
-    }
-  }
-}
-  
-  
-  
-  
-  
-  # simple breakpoint with no MATEID 
-  if (x$SVTYPE != 'BND' & is.na(x$MATEID)) { 
-    # what if CIPOS DNE?
-    #coordsA <- adjust_coordinates(x, 'CIPOS', x$POS, x$POS)
-    #coordsB <- adjust_coordinates(x, 'CIEND', x$END, x$END)
-    
-    #bedpe_row <- data.frame(CHROM_A = x$CHROM, START_A = coordsA[1] , END_A = coordsA[2],
-    #                       CHROM_B = x$CHR2, START_B = coordsB[1], END_B = coordsB[2],
-    #                        ID = x$ID, QUAL = x$QUAL, STRAND_A = '+', STRAND_B = '-')
-  
-  } else if (x$SVTYPE == 'BND' & !is.na(x$MATEID)) { 
-    mate_id <- x$MATEID
-    if (mate_id %in% bnds) {
-      mate <- bnds[['mate_id']];
-      bedpe_row <- convert_row_vcf2bedpe(mate, x);
-    } else if (mate_id %in% sec_bnds) {
-      mate <- sec_bnds[['mate_id']];
-      bedpe_row <- convert_row_vcf2bedpe(x, mate);
-    } else { # make sure var1 is smaller that var2 - coordinates-wise
-      intX <- as.integer(paste0(gsub('chr', '', x$CHROM), x$POS));
-      intY <- as.integer(paste0(gsub('chr', '', x$CHROM), y$POS));
-      if (intX < intY) {
-        bnds[[x$ID]] <- x
-      } else{
-        sec_bnds[[x$ID]] <- x
-      }
-    }
-    
-    
-    
-    bedpe_row <- data.frame(CHROM_A = x$CHROM, START_A = coordsA[1] , END_A = coordsA[2],
-                            CHROM_B = x$CHR2, START_B = coordsB[1], END_B = coordsB[2],
-                            ID = x$ID, QUAL = x$QUAL, STRAND_A = '+', STRAND_B = '-')
-    
-
-    
-  } else if () { #check ALT field
-    
-  }
-}
-
-adjust_coordinates <- function(x, info_tag, start, end) {
-  start <- integer(start)
-  end <- integer(end)
-  
-  span <- as.integer(unlist(str_split(x[['info_tag']], ",")))
-  if (length(span) != 2) {
-    warnings(paste('Invalid value for', info_tag, 'tag. Require 2 values to adjust coordinates', sep = ' '))
-    return(c(start, end))
-  } else {
-    return(c(max(start + span[1],0 ), max(end + span[2], 0)))
-  }
-}
-
-
-# parse ALT string
-parse_alt_string <- function(alt_string) {
-  coords <- unlist(str_split(alt_string, '\\[|\\]'))[2] %>% str_split(':')
-  if (grepl('].*]', alt_string)) {
-    strand <- '+'
-  } else if (grepl('[.*[', alt_string)) {
-    strand <- '-'
-  } else {stop(paste0('Unexpected ALT field: ', alt_string)) }
-
-  chrom <- as.integer(gsub('chr', '', coords[[1]][1]))
-  pos <- as.integer(coords[[1]][2])
-
-  return(list('chrom' = chrom, 'pos' = pos, 'strand' = strand ))
-}
-
-
-### DATAFRAME APPROACH
+### DATAFRAME APPROACH ====
 vcf2bedpe <- function(x, filename = NULL, header = FALSE, other = NULL, verbose = TRUE) {
   catv("CONVERT VCF TO BEDPE\n")
-  CHROM_A = x$CHROM, START_A = coordsA[1] , END_A = coordsA[2],
-  CHROM_B = x$CHR2, START_B = coordsB[1], END_B = coordsB[2],
-  ID = x$ID, QUAL = x$QUAL, STRAND_A = '+', STRAND_B = '-')
+  
   if (!is.null(attr(x, "vcf")) && attr(x, "vcf") && all(names(x) == c("header","vcf"))) {
     x <- x$vcf;
   }
@@ -203,13 +69,20 @@ vcf2bedpe <- function(x, filename = NULL, header = FALSE, other = NULL, verbose 
   
   x$CHROM <- gsub('chr', '', x$CHROM)
   
+  if (!'SVTYPE' %in% names(x)) {
+    stop('SVTYPE column must exist in VCF file')
+  }
   simple_bp <- subset(x, SVTYPE != 'BND' & is.na(x$MATEID))
   
-  if (nrow(simple_bp > 0)){
-    print('PROCESSING SIMPLE BREAKENDS \n')
+  if (nrow(simple_bp) > 0) {
+    print('PROCESSING SIMPLE BREAKENDS')
     coordsA <- adjust_coordinates(simple_bp, 'CIPOS', simple_bp$POS, simple_bp$POS)
     coordsB <- adjust_coordinates(simple_bp, 'CIEND', simple_bp$END, simple_bp$END)
     name <- get_name(simple_bp)
+    
+    if (!'CHR2' %in% names(simple_bp)) {
+      simple_bp$CHR2 <- simple_bp$CHROM
+    }
     
     simple_bedpe <- data.frame(CHROM_A = simple_bp$CHROM, 
                                START_A = coordsA$start,
@@ -218,19 +91,20 @@ vcf2bedpe <- function(x, filename = NULL, header = FALSE, other = NULL, verbose 
                                START_B = coordsB$start,
                                END_B = coordsB$end,
                                ID = name,
-                               QUAL = simple_bp$QUAL,
-                               STRAND_A = '+',
-                               STRAND_B = '_',
-                               SVTYPE = simple_bnd$SVTYPE)
+                               QUAL = ifelse(is.na(simple_bp$QUAL), '.', simple_bp$QUAL),
+                               STRAND_A = rep('+', length(simple_bp$CHROM)),
+                               STRAND_B = rep('_', length(simple_bp$CHR2)),
+                               SVTYPE = simple_bp$SVTYPE)
   }
   
   bnd_bp <- subset(x, SVTYPE == 'BND')
   if (nrow(bnd_bp > 0)) {
-    print('PROCESSING BND BREAKENDS \n')
+    print('PROCESSING BND BREAKENDS')
     
     rownames(bnd_bp) <- bnd_bp$ID
     
     if ('MATEID' %in% names(bnd_bp)) {
+      bnd_bp <- subset(bnd_bp, !is.na(MATEID))
       mates <- bnd_bp[bnd_bp$MATEID,]
 
       coordsA <- adjust_coordinates(bnd_bp, 'CIPOS', bnd_bp$POS,  bnd_bp$POS)
@@ -244,7 +118,7 @@ vcf2bedpe <- function(x, filename = NULL, header = FALSE, other = NULL, verbose 
                               CHROM_B = mates$CHROM, START_B = coordsB$start, END_B = coordsB$end,
                               ID = name, QUAL = bnd_bp$QUAL, STRAND_A = strandA, STRAND_B = strandB,
                               SVTYPE = svtype)
-      bnd_bedpe <- remove_duplicate_bnds(bnd_bedpe)
+      bnd_bedpe <- remove_duplicate_bnds(bnd_bedpe) 
     } else {
       stop('MATEID is not present in the VCF file INFO field')
     }
@@ -252,27 +126,42 @@ vcf2bedpe <- function(x, filename = NULL, header = FALSE, other = NULL, verbose 
   
   bedpe_df <- rbind(simple_bedpe, bnd_bedpe) 
   bedpe_df <- bedpe_df[with(bedpe_df, order(CHROM_A, START_A)), ]
+  
+  if (!is.null(filename)) {
+    write.table(bedpe_df, 
+                file = filename,
+                col.names = TRUE, 
+                row.names = FALSE, 
+                sep = '\t',  
+                quote = FALSE
+                )
+  }
   return(bedpe_df)
 }
 
 remove_duplicate_bnds <- function(bedpe) {
-  bedpe2 <- subset(bedpe, CHROM_A < CHROM_B)
-  bedpe3 <- subset(bedpe, CHROM_A == CHROM_B) %>%
-    subset(bedpe, START_A < START_B)
+  bedpe$CHR_A_INT <- as.integer(factor(bedpe$CHROM_A, level = unique(bedpe$CHROM_A)))
+  bedpe$CHR_B_INT <- as.integer(factor(bedpe$CHROM_B, level = unique(bedpe$CHROM_B)))
+  
+  bedpe2 <- subset(bedpe, CHR_A_INT < CHR_B_INT)
+  bedpe3 <- subset(bedpe, CHR_A_INT == CHR_B_INT & START_A < START_B)
+  #bedpe3 <- subset(bedpe, CHROM_A == CHROM_B) %>%
+  #  subset(bedpe, START_A < START_B)
   # check if same chr and same start exist
   same <- subset(bedpe, CHROM_A == CHROM_B & START_A == START_B)
   if (nrow(same) != 0) {
     warning('The following rows have the same CHROM and START value \n')
     print(same)
   }
-  return(rbind(bedpe2, bedpe3))
+  return(subset(bedpe2, select = -c(CHR_A_INT, CHR_B_INT)))
+  #return(rbind(bedpe2, bedpe3))
 }
 
-get_strand<- function(df) {
-  strand <- '+'
+get_strand <- function(df) {
+  strand <- rep('+', nrow(df))
   if ('STRAND' %in% names(df)) {
     strand <- df$STRAND
-  } else if any(grepl('\\]|\\[', df$ALT)) {
+  } else if (any(grepl('\\]|\\[', df$ALT))) {
     strand[grepl('\\[.*\\[', df$ALT)] <- '-'
   } else {
     warning('STRAND information is not recorded in INFO:STRAND or ALT field. Returning default: + ')
@@ -286,19 +175,20 @@ get_name <- function(df) {
   if ('EVENT' %in% names(df)) {
     name <- df$EVENT
   } else if (any(grepl('^Manta', name))) {
-    name <- do.call(rbind, str_split(z$ID , ':.$'))[,1]
-  } else if (any((grepl('_[12]$', name))) {
-    name <- do.call(rbind, gsub('_[12]$', '', z$ID))[,1]
-  } 
+    name <- do.call(rbind, str_split(df$ID , ':.$'))[,1]
+  } else if (any(grepl('_[12]$', name))) {
+    name <- gsub('_[12]$', '', df$ID)
+  }
   return(name)
 }
 
+
 adjust_coordinates <- function(df, info_tag, start, end) {
   # convert adjustments to numeric
-  df$start <- df[[start_col]]
-  df$end <- df[[end_col]]
+  df$start <- as.integer(start)
+  df$end <- as.integer(end)
   
-  if (!info_tag %in% names(x)) {
+  if (!info_tag %in% names(df)) {
     warning(paste0('info tag ', info_tag, ' not found in VCF file. Coordinates are not adjusted'))
     return(list('start' = df$start, 'end' = df$end))
   }
@@ -311,20 +201,23 @@ adjust_coordinates <- function(df, info_tag, start, end) {
   df$ci_start <- do.call(rbind, lapply(df[[info_tag]], as.numeric))[,1]
   df$ci_end <- do.call(rbind, lapply(df[[info_tag]], as.numeric))[,2]
   
-  df$start <- df[[start_col]] + df$ci_start
+  df$start <- df$start + df$ci_start
   df$start <- ifelse(df$start >= 0, df$start, 0)
-  df$end <- df[[end_col]] + df$ci_end
+  df$end <- df$end + df$ci_end
   df$end <- ifelse(df$end >= 0, df$end, 0)
   
   return(list('start' = df$start, 'end' = df$end))
 }                           
      
 bedpe.list <- list()
-for (i in 1:length(vcf.list)) {
-  bedpe.list[[names(vcf.list)[i]]] <- vcf2bedpe(vcf.list[[i]])
-  
-}
 
+for (i in 1:length(vcf.list)) {
+  print(paste0('Running ', names(vcf.list)[i]))
+  if (names(vcf.list)[i] == 'manta') {
+    vcf.list[[i]]$vcf$SVTYPE <- gsub('(Manta)([A-Z]{3})(:.*)', '\\2', vcf.list[[i]]$vcf$ID)
+  }
+  bedpe.list[[names(vcf.list)[i]]] <- vcf2bedpe(vcf.list[[i]])
+}
 
 
 
