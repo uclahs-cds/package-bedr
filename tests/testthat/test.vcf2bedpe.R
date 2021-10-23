0a31073f1165848cbe0bffa8e1a828140aec61e1
Helena Winata <hwinata@mednet.ucla.edu>
2021-10-23 14:03:07 -0700

    rearrange data for testing, examples, and function

--- tests/testthat/test.vcf2bedpe.R
+++ tests/testthat/test.vcf2bedpe.R
@@ -9,31 +9,31 @@
 # If publications result from research using this SOFTWARE, we ask that the Ontario Institute for Cancer Research be acknowledged and/or
 # credit be given to OICR scientists, as scientifically appropriate.
 
-if (check.binary('vcf2bedpe', verbose = TRUE)) {
-
-	test_that('check_vcf2bedpe', {
-		for (sv.caller in c('gridss', 'delly', 'manta')){
-			vcf.file <- system.file(paste0('data/', sv.caller, 'SV.vcf.gz'), package = 'bedr');
-			vcf <- read.vcf(vcf.file, split.info = TRUE);
-			bedpe <- vcf2bedpe(vcf);
-
-			# check bedpe is not empty
-			expect_equal((nrow(bedpe) > 0), TRUE);
-
-			# check the header is included
-			expect_equal(colnames(bedpe), c('CHROM_A', 'START_A', 'END_A', 'CHROM_B', 'START_B', 'END_B', 'ID', 'QUAL', 'STRAND_A', 'STRAND_B', 'SVTYPE')); 
-
-			# if SVTYPE == BND, must have MATEID
-			if (sv.caller == 'gridss') {
-				vcf$vcf <- subset(vcf$vcf, select = -MATEID);
-				expect_error(vcf2bedpe(vcf));
-			}
-		# expect error if vcf is unpacked 
-		expect_error(vcf2bedpe(vcf$vcf));
-
-		# expect error if VCF doesnt have SVTYPE column
-		vcf$vcf <- subset(vcf$vcf, select = -SVTYPE);
-		expect_error(vcf2bedpe(vcf));	
-		});
-	}
-}
+test_that('check_vcf2bedpe', {
+  if (check.binary('vcf2bedpe', verbose = TRUE)) {
+  	for (sv.caller in c('gridss', 'delly', 'manta')){
+  		vcf.file <- system.file(paste0('data/', sv.caller, 'SV.vcf.gz'), package = 'bedr');
+  		vcf <- read.vcf(vcf.file, split.info = TRUE);
+  		bedpe <- vcf2bedpe(vcf);
+  
+  		# check bedpe is not empty
+  		expect_equal((nrow(bedpe) > 0), TRUE);
+  
+  		# check the header is included
+  		expect_equal(colnames(bedpe), c('CHROM_A', 'START_A', 'END_A', 'CHROM_B', 'START_B', 'END_B', 'ID', 'QUAL', 'STRAND_A', 'STRAND_B', 'SVTYPE')); 
+  
+  		# if SVTYPE == BND, must have MATEID
+  		if (sv.caller == 'gridss') {
+  			vcf$vcf <- subset(vcf$vcf, select = -MATEID);
+  			expect_error(vcf2bedpe(vcf));
+  		}
+    	# expect error if vcf is unpacked 
+    	expect_error(vcf2bedpe(vcf$vcf));
+    
+    	# expect error if VCF doesnt have SVTYPE column
+    	vcf$vcf <- subset(vcf$vcf, select = -SVTYPE);
+    	expect_error(vcf2bedpe(vcf));	
+    	}
+    }
+  })
+  
